159e01cda1a71950fbaae5a4035a19ccaa52dfa9
Matthew Wong <matthew.raymond.wong@gmail.com>
2020-08-25 05:07:05 +0700

    edit conditional for backEndUrl

--- src/function-library/setBackendUrl.js
+++ src/function-library/setBackendUrl.js
@@ -1,4 +1,5 @@
 let backendUrl = ''
+console.log(process.env.NODE_ENV)
 console.log("conditional", process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development')
 //pushing to heroku on master branch will cause NODE_ENV to default to 'productions'
 //pushing to heroku on non-master branch will cause NODE_ENV to default to 'development'
