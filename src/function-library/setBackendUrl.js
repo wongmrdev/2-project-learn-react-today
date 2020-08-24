8a4fdce49b3d11e00bdea0552167e6a99e93665e
Matthew Wong <matthew.raymond.wong@gmail.com>
2020-08-25 05:05:46 +0700

    edit conditional for backEndUrl

--- src/function-library/setBackendUrl.js
+++ src/function-library/setBackendUrl.js
@@ -2,7 +2,7 @@ let backendUrl = ''
 console.log("conditional", process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development')
 //pushing to heroku on master branch will cause NODE_ENV to default to 'productions'
 //pushing to heroku on non-master branch will cause NODE_ENV to default to 'development'
-if(process.env.NODE_ENV === 'production' && process.env.NODE_ENV === 'development'){ 
+if(process.env.NODE_ENV === 'production'){ 
     backendUrl = 'https://desolate-inlet-08825.herokuapp.com'
     } else {
     backendUrl = 'http://localhost:5002' //port XXXX set in family-recipe-app-recipeServer/.env -->PORT=5002
