df8266a9da9848d5ec0d6612aa4acac8e5858410
Matthew Wong <matthew.raymond.wong@gmail.com>
2020-08-25 22:30:46 +0700

    backendUrl Debug

--- src/function-library/setBackendUrl.js
+++ src/function-library/setBackendUrl.js
@@ -2,6 +2,7 @@
 //pushing to heroku on master branch will cause NODE_ENV to default to 'productions' ?
 //pushing to heroku on non-master branch will cause NODE_ENV to default to 'development' ?
 let backendUrl = ''
+console.log(process.env.NODE_ENV)
 if(process.env.NODE_ENV === 'production') {
     backendUrl = 'https://desolate-inlet-08825.herokuapp.com/'
 } else
