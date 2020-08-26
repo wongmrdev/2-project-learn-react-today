ff068afbdc9b43510ac940ce1942ca0b84f82e5c
Matthew Wong <matthew.raymond.wong@gmail.com>
2020-08-27 00:29:19 +0700

    remove \ from backendUrl domiain ending

--- src/function-library/setBackendUrl.js
+++ src/function-library/setBackendUrl.js
@@ -4,7 +4,7 @@
 let backendUrl = ''
 console.log(process.env.NODE_ENV)   
 if(process.env.NODE_ENV === 'development') {
-    backendUrl = 'https://desolate-inlet-08825.herokuapp.com/'
+    backendUrl = 'https://desolate-inlet-08825.herokuapp.com'
 } else
 {backendUrl = 'http://localhost:5002' //process.env.BACKEND_URL
 }
