9b7b2691c090d7cbcb0a5580b7f13ff9af3c0be0
Matthew Wong <matthew.raymond.wong@gmail.com>
2020-08-31 04:10:08 +0700

    revise config for production url

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export const setBackendUrl = () =>  {
     if(process.env.NODE_ENV !== 'production') {
         return 'http://localhost:5002' 
     } else {
-        return 'https://secure-hollows-38145.herokuapp.com'
+        return 'https://desolate-inlet-08825.herokuapp.com/'
     }
 
 }
